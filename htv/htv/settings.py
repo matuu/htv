c152391f0f42c608cd77bcbbb19f659c845b6039
matuu <matu.varela@gmail.com>
2016-06-03 01:29:30 -0300

    ajustes para deploy

--- htv/htv/settings.py
+++ htv/htv/settings.py
@@ -9,24 +9,31 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
+from os.path import abspath, basename, dirname, join, normpath
+from os import environ
+from sys import path
 
-import os
+########## PATH CONFIGURATION
+# Absolute filesystem path to the Django project directory:
+DJANGO_ROOT = dirname(dirname(abspath(__file__)))
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Absolute filesystem path to the top-level project folder:
+SITE_ROOT = dirname(DJANGO_ROOT)
 
+# Site name:
+SITE_NAME = basename(DJANGO_ROOT)
+
+# Add our project to our pythonpath, this way we don't need to type our project
+# name in our dotted import paths:
+path.append(DJANGO_ROOT)
+########## END PATH CONFIGURATION
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0uu&rt+mduxb03*6_tvzzyk9%@s@&d7***5vx#l-5-lk=(*r58'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ADMINS = [
+    ('Matias Varela', 'matu.varela@gmail.com'),
+]
 
 # Application definition
 
@@ -82,7 +89,7 @@ WSGI_APPLICATION = 'htv.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': join(DJANGO_ROOT, 'db.sqlite3'),
     }
 }
 
@@ -124,12 +131,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = normpath(join(SITE_ROOT, '../media'))
+STATIC_ROOT = normpath(join(SITE_ROOT, '../collected_static'))
 
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "asgi_redis.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
         "ROUTING": "twitter_app.routing.channel_routing",
     },
